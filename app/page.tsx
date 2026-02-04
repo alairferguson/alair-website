9594278edda6a529bb2269b858303516eddbd379
Alex Safayan <alex@safayan.com>
2026-02-04 15:01:58 -0600

    whoops remove dup papers

--- app/page.tsx
+++ app/page.tsx
@@ -21,36 +21,6 @@ export default function Home() {
                         </ol>
                     </div>
                 </Paper>
-                <Paper>
-                    <div className="h-full w-full grid grid-rows-3 place-items-center">
-                        <Image src="/seal.png" alt="Ferguson Seal" width={120} height={120} className="select-none pointer-events-none" />
-                        <h1 className="text-center font-heading mix-blend-multiply uppercase">
-                            <span className="name-line text-7xl text-primary">Alair </span>
-                            <span className="name-line text-3xl tracking-wide">Ferguson </span>
-                            <span className="text-3xl tracking-wide">Hautzinger</span>
-                        </h1>
-                        <ol className="list-none text-xl sm:text-base list-inside text-center text-[rgba(0,0,0,0.85)] mix-blend-multiply">
-                            <li><Email /></li>
-                            <li><a href="/resume.pdf" className="hover:text-primary">II. Resume</a></li>
-                            <li><a href="https://www.linkedin.com/in/alairferguson/" className="hover:text-primary">III. LinkedIn</a></li>
-                        </ol>
-                    </div>
-                </Paper>
-                <Paper>
-                    <div className="h-full w-full grid grid-rows-3 place-items-center">
-                        <Image src="/seal.png" alt="Ferguson Seal" width={120} height={120} className="select-none pointer-events-none" />
-                        <h1 className="text-center font-heading mix-blend-multiply uppercase">
-                            <span className="name-line text-7xl text-primary">Alair </span>
-                            <span className="name-line text-3xl tracking-wide">Ferguson </span>
-                            <span className="text-3xl tracking-wide">Hautzinger</span>
-                        </h1>
-                        <ol className="list-none text-xl sm:text-base list-inside text-center text-[rgba(0,0,0,0.85)] mix-blend-multiply">
-                            <li><Email /></li>
-                            <li><a href="/resume.pdf" className="hover:text-primary">II. Resume</a></li>
-                            <li><a href="https://www.linkedin.com/in/alairferguson/" className="hover:text-primary">III. LinkedIn</a></li>
-                        </ol>
-                    </div>
-                </Paper>
             </main>
         </div>
     );
